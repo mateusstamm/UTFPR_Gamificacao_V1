914d81a2d312784f51cda15d254d5db1913eb290
Mateus <mateusstamm@alunos.utfp.edu.br>
2023-04-12 03:27:06 -0300

    Update Index.cshtml.cs

--- UTFPR_Gamificacao_V1.RazorPages/Pages/Mesas/Index.cshtml.cs
+++ UTFPR_Gamificacao_V1.RazorPages/Pages/Mesas/Index.cshtml.cs
@@ -6,20 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using UTFPR_Gamificacao_V1.RazorPages.Models;
+using UTFPR_Gamificacao_V1.RazorPages.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace UTFPR_Gamificacao_V1.RazorPages.Pages.Mesas
 {
     public class Index : PageModel
     {
         private readonly ILogger<Index> _logger;
+        private readonly AppDbContext _context;
 
-        public Index(ILogger<Index> logger)
+        public Index(ILogger<Index> logger, AppDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
+        public IList<MesaModel>? Mesas { get; set; }
+
+        /*public async Task OnGetAsync()
         {
-        }
+            Mesas = await _context.Mesas.ToListAsync();
+        }*/
     }
 }
